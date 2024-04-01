5ae0d46333a63958fc1d4c6c4fb398b0960c31e5
iusmmf <nyamato@mori-m-foundation.or.jp>
2024-04-01 21:36:19 +0900

    edit

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 var config = {
     style: "mapbox://styles/mapbox/satellite-v9",
     accessToken: 'pk.eyJ1IjoibnlhbWF0byIsImEiOiJja2Y4dzNkOW8wY3MwMnFvM29iNnJzNzVzIn0.GHlHwu3r5YjKBU3qAKvccQ',
-    showMarkers: true,
+    showMarkers: false,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
     //Read more about available projections here
     //https://docs.mapbox.com/mapbox-gl-js/example/projections/
-    inset: true,
+    inset: false,
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Okinawa Vision - Interactive Storytelling',
-    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    byline: 'By a Digital Storyteller',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    title: '沖縄県 産業・観光グランドデザイン',
+    //subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    //byline: 'By a Digital Storyteller',
+    //footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
-            image: './images/test/hibiscus.jpeg',
+            title: '沖縄県 産業・観光グランドデザイン',
+            //image: './images/test/hibiscus.jpeg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
                 center: [127.62592, 26.36929],
@@ -49,8 +49,8 @@ var config = {
             id: 'second-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Second Title',
-            image: './images/test/kokusai_dori.jpeg',
+            title: '陸・海・空のアクセス改善を実現する',
+            image: './images/test/yacht.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [127.76376, 26.71117],
@@ -74,8 +74,8 @@ var config = {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
-            image: './images/test/turtle.jpeg',
+            title: 'エリアに適したコンテンツの高付加価値化を実現する',
+            image: './images/test/kokusai_dori.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [127.66113, 26.20326],
@@ -93,12 +93,31 @@ var config = {
             id: 'fourth-chapter',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: '沖縄における観光業の構造転換を実現する',
             image: './images/test/yanbaru.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
                 center: [128.16697, 26.68819],
                 zoom: 12.07,
+                pitch: 45.00,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fifth-chapter',
+            alignment: 'left',
+            hidden: false,
+            title: '観光からの利益を環境保護に還元する',
+            image: './images/test/turtle.jpeg',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [125.18580, 24.82958],
+                zoom: 12.56,
                 pitch: 45.00,
                 bearing: 0.00
             },
